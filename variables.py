8515c61a6504a46cb36a2f85082c24d7839ecb50
saanvi-chalasani <163747779+saanvi-chalasani@users.noreply.github.com>
2026-05-28 17:21:07 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,153 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(7+9)
+16
+a=10
+print(a)
+10
+b=30
+print(b)
+30
+x=100
+print(X)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+print(x)
+100
+z=50
+print(z)
+50
+5=10
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+7=100
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a7=100
+print(a7)
+100
+20a=40
+SyntaxError: invalid decimal literal
+a0123456789=30
+print(a0123456789)
+30
+a0123=20 30  40
+SyntaxError: invalid syntax
+a01=20 30 40
+SyntaxError: invalid syntax
+name="pooja"
+print(name)
+pooja
+place="vja"
+print(place)
+vja
+country="india"
+print(country)
+india
+#=20
+$=30
+SyntaxError: invalid syntax
+_a=80
+print(_a)
+80
+ x=100
+ 
+SyntaxError: unexpected indent
+_100
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    _100
+NameError: name '_100' is not defined
+_=100
+print(_)
+100
+if=30
+SyntaxError: invalid syntax
+else=90
+SyntaxError: invalid syntax
+a=3,b=8
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=3;b=8
+print(a+b)
+11
+print(a-b)
+-5
+a=345678
+print(a)
+345678
+a=3,4,5,6,7,8,
+print(a)
+(3, 4, 5, 6, 7, 8)
+a,b,c,=9
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    a,b,c,=9
+TypeError: cannot unpack non-iterable int object
+a=b=c=20
+print (a,b,c)
+20 20 20
+a=1=2=40+8
+SyntaxError: cannot assign to literal
+a,b,c=7,8,9,
+print (a)
+7
+print(c)
+9
+a1=1,3
+print(a)
+7
+first name ="pooja"
+SyntaxError: invalid syntax
+first_name ="pooja"
+print(first_name )
+pooja
+firstname="pooja"
+print(firstname)
+pooja
+>>> name="pooja"
+>>> print(name)
+pooja
+>>> fname="pooja"
+>>> print(fname)
+pooja
+>>> fname=pooja
+Traceback (most recent call last):
+  File "<pyshell#66>", line 1, in <module>
+    fname=pooja
+NameError: name 'pooja' is not defined
+>>> fname="pooja"
+>>> lname="ch"
+>>> print(fname+lname)
+poojach
+>>> print(fname,lname)
+pooja ch
+>>> print(fname" " lname)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(lname " " fname)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(fname+ " " lname)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> a.b.c.=(6,7,8)
+SyntaxError: invalid syntax
+>>> a,b,c=(7,8,9,)
+>>> print(a,b,c)
+7 8 9
+>>> x=10
+>>> print(x)
+10
+>>> del x
+>>> print(x)
+Traceback (most recent call last):
+  File "<pyshell#80>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+>>> Age=10
+>>> print(Age)
+10
+>>> AGE=30
+>>> print(AGE)
+30
+>>> age=70
+>>> print(age)
+70
