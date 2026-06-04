f9cb88db58782910cd43b9d81889605bda875d4c
saanvi-chalasani <163747779+saanvi-chalasani@users.noreply.github.com>
2026-06-04 10:53:20 +0530

    Add files via upload

---
+++ compiler.py 1.py
@@ -0,0 +1,67 @@
+'''a=10
+b=20
+print(a+b)'''
+
+'''a=3
+b=7
+c=a+b
+print(c)'''
+
+#run_time input()
+
+'''a=int(input("Enter a Value"))
+b=int(input("Enter a Value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("enter the a value"))
+b=str(input("enter the b value"))
+print(a+b)'''
+
+'''a=input("enter the a value")
+b=input("enter the b value")
+print(a+b)'''
+
+'''fname=input("enter first name,""\n")
+lname=input("enter last name")
+print(fname +lname)'''
+
+''''a=complex(input("data 1"))
+b=complex(input("data 2"))
+print(a+b)'''
+
+'''a=bool(input("data1"))
+b=bool(input("data2"))
+print(a+b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the options 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the options
+                 1.add
+                 2.sub
+                 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+
+      
+                 
+                 
+
+
+ 
+
+
+      
